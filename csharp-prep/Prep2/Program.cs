f14a5eac91665cb5db8b178eb6b9c5dc34571403
sarahsteadman <fairyfaye4real@gmail.com>
2023-01-04 23:26:06 -0700

    Grading project

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is our grade percentage?");
+        string stringGrade = Console.ReadLine();
+        int intGrade = int.Parse(stringGrade);
+
+        string letter = "A monkey's butt!";
+
+        if (intGrade >= 90)
+        {
+             letter = "A";
+        }
+        else if (intGrade >= 80)
+        {
+             letter = "B";
+        }else if (intGrade >= 70)
+        {
+             letter = "C";
+        }else if (intGrade >= 60)
+        {
+             letter = "D";
+        }else
+        {
+             letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (intGrade > 70)
+        {
+            Console.WriteLine("You passed! Hooray!");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass this course. Please retake it.");
+        }
     }
 }
