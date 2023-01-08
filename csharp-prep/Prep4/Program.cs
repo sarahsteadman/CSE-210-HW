4a441135f0fc67326ca8a5b6de497af00c396fad
sarahsteadman <fairyfaye4real@gmail.com>
2023-01-07 23:06:48 -0700

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        List<float> numbers = new List<float>();
+        float answer = 8;
+        float sum = 0;
+        float amount = -1;
+        float largest = -999999999;
+        while (answer != 0)
+            {
+                Console.WriteLine("Enter a number: ");
+                string answer_string = Console.ReadLine();
+                answer = float.Parse(answer_string);
+
+                numbers.Add(answer);
+
+            }
+
+        foreach (float number in numbers)
+            {
+                sum = sum + number;
+                amount = amount + 1;
+                if (number > largest)
+                    {
+                        largest = number;
+                    }
+            }
+
+        float average = sum / amount;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
